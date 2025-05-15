5e8ab8eea607bc4a33ae0ad4d675dedc848ba154
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2025-05-15 10:45:04 +0530

    Added my Day 1 DSA code

---
+++ First.js
@@ -0,0 +1,112 @@
+// Sum of two integer
+// let a = 10;
+// let b = 20;
+// console.log(a+b);   ans = 30
+
+// Relation between integer and string
+// let a = 20;
+// let b = "10";
+// console.log(typeof(a + b)); ans = string
+
+// Concatenate
+// let a = 20;
+// let b = 20;
+// console.log("Sum of 20 and 20 = " + a + b);  ans = 2020
+
+// let a = 10;
+// let b = 20;
+// console.log("sum of 10 and 20 = " + (a + b));  ans = 30
+
+// let a = 15;
+// let b = 20;
+// console.log(a + b + " is sum of 15 and 20");  ans = 35
+
+// Type Coercion
+// console.log("10" - 5);  ans = 5
+
+// let age = prompt("enter your age")
+// console.log(typeof(age));  ans = String
+
+// Type casting / Type conversion
+// let age = Number(prompt("enter your age"))
+// console.log(age);   ans = Number
+
+// Swapping of number
+// let a = 10;
+// let b = 20;
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log(a);  ans = 20
+// console.log(b);  ans = 10
+
+// second way of Swapping
+// let a = 10;
+// let b = 20;
+
+// a = a + b;
+// b = a - b
+// a = a - b
+
+// console.log(a); ans = 20
+// console.log(b); ans = 10
+
+// Third way of swapping or
+// Array Destructuring Assignment / Destructuring Assignment
+// let a = 10;
+// let b = 20;
+
+// [a, b] = [b, a]
+
+// console.log(a, b); ans = a - 20, b - 10
+
+// arithmetic operator
+// let a = 5676;
+// console.log(a % 10); ans = 6
+
+// let a = 7979;
+// console.log(a % 100); ans = 79
+
+// Math Function
+// console.log(Math.round(5.4));
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+// console.log(Math.trunc(5.9));
+// console.log(Math.pow(3,4));
+// console.log(Math.sqrt(25));
+// console.log(Math.cbrt(27));
+// console.log(Math.abs(-5.8));
+// console.log(Math.max(5.1, -3, 10));
+// console.log(Math.min(5.8, -3, 10));
+// console.log(Math.random());
+
+// Question
+
+// let i = 20;
+// i = i++ + ++i;
+// console.log(i);
+
+// let a = 11
+// let b = 22
+// let c = a + b + a++ + b++ + ++a + ++b
+// console.log("a =" + a);
+// console.log("b ="+ b);
+// console.log("c ="+ c);
+
+// let a = true
+// a++
+// console.log(a);
+
+// let a = false
+// a++
+// console.log(a);
+
+// let a = 10++
+// console.log(a);
+
+// let a = 10
+// let b = ++(a++)
+// console.log(b);
