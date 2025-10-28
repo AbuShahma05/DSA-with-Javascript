b0a5edb75386c194f9dcca84ba65baa31097ef88
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2025-10-28 19:33:27 +0530

    day 56 of dsa journey

---
+++ day56.js
@@ -0,0 +1,26 @@
+/* 
+
+write a function that searches for an element inside an array and returns the index ,
+if the element is not present then just return -1
+
+*/
+let arr = [4, 2, 0, 10, 8, 30];
+let findnum = 10;
+
+function searchElement(arr) {
+  let found = false;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === findnum) {
+      console.log("Found at index", i);
+      found = true;
+      break;
+    }
+  }
+
+  if(!found){
+    console.log(-1);
+    
+  }
+}
+
+searchElement(arr);
