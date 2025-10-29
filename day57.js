39d83a9d147fd10dc61c4f27797f20d4a89058ed
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2025-10-29 21:12:08 +0530

    Day 57 of dsa journey

---
+++ day57.js
@@ -0,0 +1,41 @@
+/* 
+write a function that returns the number of negative number in an array
+*/
+
+let arr = [2, -9, 17, 0, 1, -10, -4, 8];
+
+let count = 0;
+
+function returnNegativeElementSum(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+      count++;
+    }
+  }
+
+  console.log("Number of negative number is:", count);
+}
+
+returnNegativeElementSum(arr);
+
+let arr2 = [5, 0, 10, 8, 17, 0];
+let largest = arr2[0];
+let store = 0;
+
+function largestElement(arr2) {
+  for (let i = 0; i < arr2.length; i++) {
+    if (largest < arr2[i]) {
+      largest = arr2[i];
+    }
+  }
+
+  console.log("largest element is:", largest);
+}
+
+largestElement(arr2);
+
+/* 
++infinity ??
+-infinity ??
+try to find smallest number in an array 
+*/
