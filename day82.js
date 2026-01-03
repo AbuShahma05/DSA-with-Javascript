11a0dd0b8510c7bfdc4a1496e7a6032796c38f2e
AbuShahma05 <abushahmasiddiqui880@gmail.com>
2026-01-03 10:38:55 +0530

    Day 87 Of DSA Journey

--- day82.js
+++ day82.js
@@ -2,8 +2,8 @@
 let arr = [5, 3, 2, 1, 0];
 
 function sum(n) {
-  if (n === 0) {
-    return arr[n];
+  if (n < 0) {
+    return 0;
   }
   return arr[n] + sum(n - 1);
 }
--- practice.js
+++ practice.js
@@ -1,19 +1,25 @@
-function printNm(n){
-    if(n === 0) return
+// function printNm(n){
+//     if(n === 0) return
 
-    console.log(n);
-    printNm(n-1)
+//     console.log(n);
+//     printNm(n-1)
+// }
+
+// let n = 5;
+// printNm(5)
+
+// var printNumber = function(l){
+//     if(l == 0) return
+
+//     console.log(l);
+//     printNumber(l-1)
+// }
+
+// let l = 6
+// printNumber(l)
+
+var factorial = function(n){
+    if(n == 1) return 1
+    return n*factorial(n-1)
 }
-
-let n = 5;
-printNm(5)
-
-var printNumber = function(l){
-    if(l == 0) return
-
-    console.log(l);
-    printNumber(l-1)
-}
-
-let l = 6
-printNumber(l)
+console.log(factorial(5));
